8c1f94bdf3463ecce73143d4a5227d4483f26812
Than Hutchins <mynockspit@gmail.com>
2021-11-20 09:58:26 -0700

    set up auto-release on tag

--- config/forge.config.js
+++ config/forge.config.js
@@ -2,24 +2,37 @@ module.exports = {
   packagerConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        name: 'endless_sky_data_editor_electron'
-      }
-    },
-    {
       name: '@electron-forge/maker-zip',
       platforms: [
-        'darwin'
+        'darwin', 'linux', 'win32'
       ]
     },
+    // leaving these here for now, but I don't know if they're useful
+    // they make the build take longer
+    // {
+    //   name: '@electron-forge/maker-squirrel',
+    //   config: {
+    //     name: 'endless_sky_data_editor_electron'
+    //   }
+    // },
+    // {
+    //   name: '@electron-forge/maker-deb',
+    //   config: {}
+    // },
+    // {
+    //   name: '@electron-forge/maker-rpm',
+    //   config: {}
+    // }
+  ],
+  publishers: [
     {
-      name: '@electron-forge/maker-deb',
-      config: {}
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {}
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'MynockSpit',
+          name: 'endless-sky-data-editor'
+        }
+      }
     }
   ],
   plugins: [
