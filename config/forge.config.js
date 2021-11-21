dca130c57e22b96104e97b6e77b12822b26651b8
Than Hutchins <mynockspit@gmail.com>
2021-11-20 18:54:17 -0700

    provide separate build steps and see if it publishes right (probably not?)

--- config/forge.config.js
+++ config/forge.config.js
@@ -2,41 +2,31 @@ module.exports = {
   packagerConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        name: 'endless_sky_data_editor_electron'
-      }
-    },
-    {
       name: '@electron-forge/maker-zip',
       platforms: [
         'darwin', 'linux', 'win32'
       ]
     },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {}
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {}
-    }
+    // leaving these here for now, but I don't know if they're useful
+    // they make the build take longer
+    // {
+    //   name: '@electron-forge/maker-squirrel',
+    //   config: {
+    //     name: 'endless_sky_data_editor_electron'
+    //   }
+    // },
+    // {
+    //   name: '@electron-forge/maker-deb',
+    //   config: {}
+    // },
+    // {
+    //   name: '@electron-forge/maker-rpm',
+    //   config: {}
+    // }
   ],
   publishers: [
     {
       name: '@electron-forge/publisher-github',
-      platforms: ['darwin', 'linux'],
-      config: {
-        repository: {
-          owner: 'MynockSpit',
-          name: 'endless-sky-data-editor'
-        },
-        draft: true
-      }
-    },
-    {
-      name: '@electron-forge/publisher-github',
-      platforms: ['win32'],
       config: {
         repository: {
           owner: 'MynockSpit',
